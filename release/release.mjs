348d407f061e8b3ad2c37b021c91ebca799aa365
cmidgley <chris@Koose.com>
2023-01-11 15:53:42 -0500

    Remove secondary license attribution

--- release/release.mjs
+++ release/release.mjs
@@ -24,8 +24,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- *
- * Additional license and attribution requrements are included in subsections below.
  */
 
 /*
